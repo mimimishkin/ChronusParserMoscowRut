c27c1778edc777c41a20c48f373ea7ac644f742e
mika <printf.mika@gmail.com>
2025-03-24 14:03:05 +0300

    ☆⌒(*＾-゜)v

--- appmodules/datasource/network/chronus/yourcity-youruniversity/src/commonMain/kotlin/datasource/network/chronus/yourcity/youruniversity/YouruniversityGetLessons.kt
+++ appmodules/datasource/network/chronus/yourcity-youruniversity/src/commonMain/kotlin/datasource/network/chronus/yourcity/youruniversity/YouruniversityGetLessons.kt
@@ -5,14 +5,19 @@ import com.fleeksoft.ksoup.nodes.Element
 import io.ktor.client.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.bodyAsText
+import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import model.common.parseRus
 import kotlinx.coroutines.cancel
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.supervisorScope
 import kotlinx.coroutines.withContext
+import kotlinx.coroutines.yield
 import kotlinx.datetime.Clock
 import kotlinx.datetime.DateTimeUnit
 import kotlinx.datetime.DateTimeUnit.Companion.MINUTE
@@ -42,140 +47,145 @@ import kotlin.String
 
 suspend fun getLessons(client: HttpClient, json: Json, schedule: Schedule): List<Lesson>? {
 	return try {
-		supervisorScope {
-			parseTimetable(client, schedule.url)
-		}
-	} catch (e: Exception) {
-		log(LogType.NetworkClientError, e)
-		return null
+		parseTimetable(client, schedule.url)
+	} catch (_: CancellationException) { // ловим отмену операций, причина уже в логе
+		null
 	}
 }
 
-private suspend fun parseTimetable(client: HttpClient, url: String, parseOther: Boolean = true): List<Lesson>? {
+private suspend fun parseTimetable(client: HttpClient, url: String, parseOther: Boolean = true): List<Lesson>? = coroutineScope {
+	val parsingScope = this
+
 	val page = try {
 		client.get(url).bodyAsText()
 	} catch (e: Exception) {
 		log(LogType.NetworkClientError, e)
-		return null
+		return@coroutineScope null
 	}
 
-	return withContext(Dispatchers.Default) {
-		val page = Ksoup.parse(page)
-		val timetableList = page.getElementsByClass("nav-tabs").singleOrNull()
-		if (timetableList == null)
-			return@withContext emptyList()
+	return@coroutineScope try {
+		withContext(Dispatchers.Default) {
+			val page = Ksoup.parse(page)
+			val timetableList = page.getElementsByClass("nav-tabs").singleOrNull()
+			if (timetableList == null)
+				return@withContext emptyList()
 
-		val otherLessons = if (parseOther) {
-			timetableList.getElementsByTag("a").drop(1).map { it: Element ->
-				async(Dispatchers.IO) {
-					val res = parseTimetable(client, YOUR_PLACE.defaultUrl.dropLast(1) + it.attr("href"), false)
-					if (res == null)
-						cancel()
-					res!!
-				}
-			}
-		} else emptyList()
-
-		val zone = TimeZone.of(YOUR_PLACE.city.timeZoneId)
-		val today = Clock.System.todayIn(zone)
-		val days = page.getElementsByClass("info-block_collapse")
-		val lessons = days.flatMap { day ->
-			val date = day.attribute("data-date")?.let { LocalDate.parseRus(it.value) }
-				?: day.select(".info-block__header > span > span").text().parseRusShortDate(today)
-				?: run {
-					val dayOfWeek = day.select(".info-block__header > span").text().asDayOfWeek()!!
-					val weekContainer = day.parents().firstOrNull { it.id().startsWith("week") }
-					val period = 1 + (weekContainer?.siblingElements()?.size ?: 0)
-					val week = weekContainer?.id()?.substringAfter('-')?.toInt() ?: 1
-					val activeTimetableLink = timetableList.getElementsByClass("active").first()!!.child(0)
-					val start = LocalDate.parse(activeTimetableLink.attr("href").substringAfter("start=").substringBefore('&'))
-
-					val periodsBetween = start.until(today, DateTimeUnit.WEEK) / period
-					val originalWeek = start.plus(periodsBetween * period + week - 1, DateTimeUnit.WEEK)
-					originalWeek.weekStartDay().plus(dayOfWeek.ordinal, DateTimeUnit.DAY)
-				}
-
-			if (date.daysUntil(today) <= 7) { // оставляем расписание максимум на неделю назад (если предоставляется)
-				day.getElementsByClass("timetable__list-timeslot").flatMap { slot ->
-					val (timeElement, contentElement) = slot.children()
-
-					val time = timeElement.text().substringAfter(", ")
-					val (start, end) = time.trim().split(" — ").map { it.asLocalTime()!!.atDate(date) }
-
-					contentElement.children().chunked(3).map { (title, about) ->
-						val rawType = title.text()
-						val isOnline = "Вебинар" in rawType
-						val type = (if (isOnline) rawType.replace(" (Вебинар)", "") else rawType)
-						val name = title.nextSibling()!!.toString().trim()
-
-						val professors = about.getElementsByClass("icon-academic-cap").map {
-							EntryInfo(it.attr("title"), YOUR_PLACE.defaultUrl.dropLast(1) + it.attr("href"))
-						}.toSet()
-
-						val locationHints = mutableListOf<String>()
-						val locations = if (isOnline) {
-							setOf(EntryInfo("Вебинар", null))
-						} else {
-							about.getElementsByClass("icon-location").map {
-								val hint = it.attr("title")
-								locationHints += hint
-
-								val name = it.lastChild().toString().replace("Аудитория ", "").trim()
-								val url = it.attribute("href")?.let { YOUR_PLACE.defaultUrl + it.value }
-								EntryInfo(name, url)
-							}.toSet()
-						}
-
-						val groups = about.getElementsByClass("icon-community").map {
-							val name = it.lastChild().toString().trim()
-							val url = it.attribute("href")?.let { YOUR_PLACE.defaultUrl + it.value } ?: url
-							EntryInfo(name, url)
-						}
-						val (sub, ordinal) = groups.partition { "п/гр." in it.name }
-
-						Lesson(
-							name = name,
-							type = type.let {
-								fun LessonType.asShortString(): String? = when (this) {
-									LessonType.Lecture -> "Лек."
-									LessonType.Practice -> "Пр."
-									LessonType.LabWork -> "Лаб."
-									LessonType.Project -> "Проект"
-									LessonType.Exam -> "Экз."
-									LessonType.CourseCredit -> "Зач."
-									LessonType.Consultation -> "Конс."
-									is LessonType.Other -> asString()
-								}
-
-								if (it.contains(';') || it.contains(',')) {
-									val types = it.split(';', ',').map { it.asLessonType() }
-									LessonType.Other(types.joinToString(", ") { it.asShortString()!! })
-								} else {
-									it.asLessonType()
-								}
-							},
-							startTime = start,
-							durationInMinutes = start.toInstant(zone).until(end.toInstant(zone), MINUTE).toInt(),
-							groups = ordinal.toSet(),
-							subgroups = sub.map { it.name.substringAfter('.').toInt() }.toSet(),
-							persons = professors,
-							classrooms = locations,
-							additionalInfo = locationHints.takeIf { it.isNotEmpty() }?.let {
-								if (it.size == 1) {
-									"Подсказка к аудитории: " + it[0]
-								} else {
-									"Подсказки к аудиториям:\n" + it.joinToString("\n")
-								}
-							}
-						)
+			val otherLessons = if (parseOther) {
+				timetableList.getElementsByTag("a").drop(1).map {
+					async(Dispatchers.IO) {
+						val res = parseTimetable(client, YOUR_PLACE.defaultUrl.dropLast(1) + it.attr("href"), false)
+						if (res == null) parsingScope.cancel("Failure to parse another timetable")
+						res
 					}
 				}
-			} else {
-				emptyList()
-			}
-		}
+			} else emptyList()
 
-		lessons + otherLessons.awaitAll().flatten()
+			val zone = TimeZone.of(YOUR_PLACE.city.timeZoneId)
+			val today = Clock.System.todayIn(zone)
+			val days = page.getElementsByClass("info-block_collapse")
+			val lessons = days.flatMap { day ->
+				val date = day.attribute("data-date")?.let { LocalDate.parseRus(it.value) }
+					?: day.select(".info-block__header > span > span").text().parseRusShortDate(today)
+					?: run {
+						val dayOfWeek = day.select(".info-block__header > span").text().asDayOfWeek()!!
+						val weekContainer = day.parents().firstOrNull { it.id().startsWith("week") }
+						val period = 1 + (weekContainer?.siblingElements()?.size ?: 0)
+						val week = weekContainer?.id()?.substringAfter('-')?.toInt() ?: 1
+						val activeTimetableLink = timetableList.getElementsByClass("active").first()!!.child(0)
+						val start = LocalDate.parse(activeTimetableLink.attr("href").substringAfter("start=").substringBefore('&'))
+
+						val periodsBetween = start.until(today, DateTimeUnit.WEEK) / period
+						val originalWeek = start.plus(periodsBetween * period + week - 1, DateTimeUnit.WEEK)
+						originalWeek.weekStartDay().plus(dayOfWeek.ordinal, DateTimeUnit.DAY)
+					}
+
+				if (date.daysUntil(today) <= 7) { // оставляем расписание максимум на неделю назад (если предоставляется)
+					day.getElementsByClass("timetable__list-timeslot").flatMap { slot ->
+						if (!isActive) throw CancellationException("Failure to parse another timetable")
+
+						val (timeElement, contentElement) = slot.children()
+
+						val time = timeElement.text().substringAfter(", ")
+						val (start, end) = time.trim().split(" — ").map { it.asLocalTime()!!.atDate(date) }
+
+						contentElement.children().chunked(3).map { (title, about) ->
+							val rawType = title.text()
+							val isOnline = "Вебинар" in rawType
+							val type = if (isOnline) rawType.replace(" (Вебинар)", "") else rawType
+							val name = title.nextSibling()!!.toString().trim()
+
+							val professors = about.getElementsByClass("icon-academic-cap").map {
+								EntryInfo(it.attr("title"), YOUR_PLACE.defaultUrl.dropLast(1) + it.attr("href"))
+							}.toSet()
+
+							val locationHints = mutableListOf<String>()
+							val locations = if (isOnline) {
+								setOf(EntryInfo("Вебинар", null))
+							} else {
+								about.getElementsByClass("icon-location").map {
+									val hint = it.attr("title")
+									locationHints += hint
+
+									val name = it.lastChild().toString().replace("Аудитория ", "").trim()
+									val url = it.attribute("href")?.let { YOUR_PLACE.defaultUrl + it.value }
+									EntryInfo(name, url)
+								}.toSet()
+							}
+
+							val groups = about.getElementsByClass("icon-community").map {
+								val name = it.lastChild().toString().trim()
+								val url = it.attribute("href")?.let { YOUR_PLACE.defaultUrl + it.value } ?: url
+								EntryInfo(name, url)
+							}
+							val (sub, ordinal) = groups.partition { "п/гр." in it.name }
+
+							Lesson(
+								name = name,
+								type = type.let {
+									fun LessonType.asShortString(): String? = when (this) {
+										LessonType.Lecture -> "Лек."
+										LessonType.Practice -> "Пр."
+										LessonType.LabWork -> "Лаб."
+										LessonType.Project -> "Проект"
+										LessonType.Exam -> "Экз."
+										LessonType.CourseCredit -> "Зач."
+										LessonType.Consultation -> "Конс."
+										is LessonType.Other -> asString()
+									}
+
+									if (it.contains(';') || it.contains(',')) {
+										val types = it.split(';', ',').map { it.asLessonType() }
+										LessonType.Other(types.joinToString(", ") { it.asShortString()!! })
+									} else {
+										it.asLessonType()
+									}
+								},
+								startTime = start,
+								durationInMinutes = start.toInstant(zone).until(end.toInstant(zone), MINUTE).toInt(),
+								groups = ordinal.toSet(),
+								subgroups = sub.map { it.name.substringAfter('.').toInt() }.toSet(),
+								persons = professors,
+								classrooms = locations,
+								additionalInfo = locationHints.takeIf { it.isNotEmpty() }?.let {
+									if (it.size == 1) {
+										"Подсказка к аудитории: " + it[0]
+									} else {
+										"Подсказки к аудиториям:\n" + it.joinToString("\n")
+									}
+								}
+							)
+						}
+					}
+				} else {
+					emptyList()
+				}
+			}
+
+			lessons + otherLessons.awaitAll().flatMap { it!! }
+		}
+	} catch (e: Exception) {
+		log(LogType.ParseError, e)
+		null
 	}
 }
 
